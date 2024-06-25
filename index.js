e00c27530ee38714ea0bd8bf6c514df192e0fbb6
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-06-25 16:14:42 +0300

    creates arrow functions

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = function (){
+  let a = 2000;
+  let b = 100;
+  let divide =a/b;
+  return divide;
+}
+
+const square = x => x * x;
+//console.log(square(2))
+
+const add = (a, b) => a + b
+//console.log(add(10,100))
