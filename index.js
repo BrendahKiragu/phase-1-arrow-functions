6faa7357421acc0e2b30d3ada98165e4f6340c74
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-06-28 10:35:51 +0300

    creates arithmetic equations with arrow functions

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const divide = function (){
-  let a = 2000;
-  let b = 100;
-  let divide =a/b;
-  return divide;
+const divide = function (a,b){
+  return a/b;
 }
+console.log(divide(2000, 100))
+
+//const divide = (a,b) => a/b;
+//console.log(divide(2000,100))
 
 const square = x => x * x;
-//console.log(square(2))
+//console.log (square(2))
 
-const add = (a, b) => a + b
-//console.log(add(10,100))
+const add = (a, b) => a + b;
+//console.log(add(3, 4))
